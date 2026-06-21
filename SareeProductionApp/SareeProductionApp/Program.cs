107b0d801e099788731273ad946a1d4ded06bde3
suresh-piocon <pioconsoftware@gmail.com>
2026-06-21 16:06:02 +0530

    Disable server prerendering and register HttpClient in server DI to fix bootstrapping exception

--- SareeProductionApp/SareeProductionApp/Program.cs
+++ SareeProductionApp/SareeProductionApp/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+builder.Services.AddScoped(sp => new HttpClient());
 builder.Services.AddMudServices();
 
 var app = builder.Build();
